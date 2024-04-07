b23dec4ffda305f99eb65cdbff95f3cdbc29ed06
Sharad <sharadritu.58.69@gmail.com>
2024-04-07 23:00:26 +0545

    create search api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-
+import * as RecipeAPI from './recipe-api';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/api/recipes/search", async (req, res) => {
+  const searchTerm = req.query.searchTerm as string;
+  const page = parseInt(req.query.page as string);
+  const results = await RecipeAPI.searchRecipes(searchTerm, page);
+
+  return res.json(results);
+});
 
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
